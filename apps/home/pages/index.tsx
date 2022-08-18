deec3cb052f1117a48faa68f36057cf0ff62f8b0
Mazy <landonp95@gmail.com>
2022-08-18 12:25:15 -0400

    navbar lib (will complete later)

--- apps/home/pages/index.tsx
+++ apps/home/pages/index.tsx
@@ -1,9 +1,9 @@
+import { Navbar } from "@mazynetwork/ui"
+
 export default function Index() {
   return (
     <div className="min-h-screen bg-neutral-200">
-      <p className="text-center text-5xl text-green-500">
-        Home
-      </p>
+      <Navbar />
     </div>
   )
 }
---
+++ libs/ui/src/lib/navbar/navbar.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface NavbarProps {}
+
+export function Navbar() {
+  return (
+      <div>
+        <p>Welcome to Navbar!</p>
+      </div>
+    );
+}
+
+export default Navbar;
