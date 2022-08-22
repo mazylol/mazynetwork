efd514caf6186dc9b7a3153816a6fb063a18754f
Mazy <landonp95@gmail.com>
2022-08-22 02:49:54 -0400

    different style

--- libs/ui/src/lib/footer/footer.tsx
+++ libs/ui/src/lib/footer/footer.tsx
@@ -9,16 +9,7 @@ export function Footer(props: FooterProps) {
       <p className="text-gray-300 text-2xl mx-auto my-3">
         Made with {'<3'} by{' '}
         <Link href="https://github.com/mazylol">
-          <span
-            className="bg-gradient-to-r from-blue-500 to-blue-500
-      bg-no-repeat [background-position:0_88%]
-      [background-size:100%_0.2em]
-      motion-safe:transition-all motion-safe:duration-200
-      hover:[background-size:100%_100%]
-      focus:[background-size:100%_100%] hover:text-gray-100"
-          >
-            Mazy
-          </span>
+          <span className="link-hover">Mazy</span>
         </Link>
       </p>
     </div>
--- libs/ui/src/lib/navitem/navitem.tsx
+++ libs/ui/src/lib/navitem/navitem.tsx
@@ -10,10 +10,8 @@ export function Navitem(props: NavitemProps) {
   return (
     <div>
       <Link href={props.path}>
-        <div className="my-2 px-2 mx-2 rounded-md bg-zinc-600 hover:bg-blue-500 hover:rounded-xl transition-all duration-300 group">
-          <p className="text-3xl text-gray-300 text-center group-hover:text-gray-100">
-            {props.name}
-          </p>
+        <div className="mx-2 my-2">
+          <p className="text-3xl text-gray-300 link-hover">{props.name}</p>
         </div>
       </Link>
     </div>
