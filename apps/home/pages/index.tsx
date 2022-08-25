be5c8cff208ed2298f8ad192c55afd4e2167ddfa
Mazy <landonp95@gmail.com>
2022-08-25 12:28:49 +0000

    title / subtitle

--- apps/home/pages/index.tsx
+++ apps/home/pages/index.tsx
@@ -1,6 +1,10 @@
 export default function Index() {
   return (
-    <div className="">
+    <div className="w-full mt-5">
+      <div className="text-center text-gray-200 font-semibold">
+        <p className="text-7xl">Mazylol</p>
+        <p className="text-4xl italic mt-2">A very dumb developer</p>
+      </div>
     </div>
-  )
+  );
 }
