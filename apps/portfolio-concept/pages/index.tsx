676d452a056471796f92b219a2afb9d3f63ce3aa
Mazy <landonp95@gmail.com>
2022-08-30 17:36:25 +0000

    boxes

--- apps/portfolio-concept/pages/index.tsx
+++ apps/portfolio-concept/pages/index.tsx
@@ -6,11 +6,16 @@ export function Index() {
           <div className="group-hover:bg-yellow-500 innerbox"></div>
           <p className="box-title group-hover:hidden">Me</p>
           <p className="inner-title group-hover:block">A Tiny Bit About Me</p>
+          <p className="inner-text group-hover:block">Hey, {"I'm"} Landon and I like to do a lot of stuff. Including but not limited to: Programming, Tinkering, and Networking. For the most part I make Discord Bots and Websites. Although there is a GraphQL API in the works. Have a good one!</p>
         </div>
         <div className="bg-blue-500 box group">
           <div className="group-hover:bg-blue-500 innerbox"></div>
           <p className="box-title group-hover:hidden">Skills</p>
-          <p className="inner-title group-hover:block">Some Stuff That I Do</p>
+          <p className="inner-title group-hover:block">Some Stuff That I Can Do {"(badly)"}</p>
+          <ul className="hidden group-hover:block mx-auto text-center pt-[1vh] text-neutral-600 font-medium text-xl">
+            <li>* Web Dev (TS, HTML, CSS)</li>
+            <li>* Coding with Rust, C, Python, and Kotlin</li>
+          </ul>
         </div>
         <div className="bg-red-500 box group">
           <div className="group-hover:bg-red-500 innerbox"></div>
