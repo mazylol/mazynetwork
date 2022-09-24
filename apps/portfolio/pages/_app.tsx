2b20bc338c36900a0cc7115ade126ecdf8375014
mazylol <landonp95@gmail.com>
2022-09-24 09:12:30 -0400

    meta tags

--- apps/portfolio/pages/_app.tsx
+++ apps/portfolio/pages/_app.tsx
@@ -8,6 +8,12 @@ function CustomApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Mazylol | Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="title" content="Mazylol" />
+        <meta name="description" content="A very idiotic techie that likes to do even more idiotic things." />
+        <meta name="keywords" content="programmer, networking, hardware" />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
       </Head>
       <div className="bg-neutral-400 min-h-screen">
         <main>
