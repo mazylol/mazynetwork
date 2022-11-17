15c2d8b31ad5acc1d110946fd7b4af49d4e795cd
mazylol <landonp95@gmail.com>
2022-11-16 22:45:45 -0500

    eye candy

--- apps/portfolio/pages/index.tsx
+++ apps/portfolio/pages/index.tsx
@@ -9,11 +9,21 @@ export function Index() {
           <p className="box-title group-hover:hidden">Me</p>
           <p className="inner-title group-hover:block">A Tiny Bit About Me</p>
           <p className="inner-text group-hover:block">
-            Hey, {"I'm"} Landon and I like to do a lot of stuff. Including but
-            not limited to programming random stuff, tinkering with hardware,
-            and networking. For the most part I make Discord bots, websites, and
-            stuff that makes my life easier. In my free time (which I have a lot
-            of) I like to play with computer hardware and mess with my Linux
+            Hey, my name is{' '}
+            <span className="text-yellow-500 font-semibold">Landon</span> and I
+            like to do a lot of stuff. Including but not limited to{' '}
+            <span className="text-yellow-500 font-semibold">programming</span>{' '}
+            random stuff, tinkering with{' '}
+            <span className="text-yellow-500 font-semibold">hardware</span>, and{' '}
+            <span className="text-yellow-500 font-semibold">networking</span>.
+            For the most part I make{' '}
+            <span className="text-yellow-500 font-semibold">discord bots</span>,{' '}
+            <span className="text-yellow-500 font-semibold">websites</span>, and
+            stuff that makes my life{' '}
+            <span className="text-yellow-500 font-semibold">easier</span>. In my
+            free time (which I have a lot of) I like to play with computers and
+            mess with my{' '}
+            <span className="text-yellow-500 font-semibold">Linux</span>{' '}
             configs. Cheers!
           </p>
         </div>
@@ -25,69 +35,97 @@ export function Index() {
             <li className="py-1">
               <span className="text-blue-500 font-semibold">Programming</span>{' '}
               with{' '}
-              <a href="https://kotlinlang.org/">
-                <span className="text-[#7f52ff] font-medium">Kotlin</span>
-              </a>
+              <Link href="https://kotlinlang.org/">
+                <span className="link-hover bg-gradient-to-r from-[#7f52ff] to-[#7f52ff] font-medium">
+                  Kotlin
+                </span>
+              </Link>
               ,{' '}
-              <a href="https://learn.microsoft.com/en-us/dotnet/csharp/">
-                <span className="text-[#009404] font-medium">C#</span>
-              </a>
+              <Link href="https://learn.microsoft.com/en-us/dotnet/csharp/">
+                <span className="link-hover bg-gradient-to-r from-[#009404] to-[#009404] font-medium">
+                  C#
+                </span>
+              </Link>
               ,{' '}
-              <a href="https://en.wikipedia.org/wiki/C_(programming_language)">
-                <span className="text-[#00599c] font-medium">C</span>
-              </a>
+              <Link href="https://en.wikipedia.org/wiki/C_(programming_language)">
+                <span className="link-hover bg-gradient-to-r from-[#00599c] to-[#00599c] font-medium">
+                  C
+                </span>
+              </Link>
               ,{' '}
-              <a href="https://go.dev">
-                <span className="text-[#007d9c] font-medium">Go</span>
-              </a>
+              <Link href="https://go.dev">
+                <span className="link-hover bg-gradient-to-r from-[#007d9c] to-[#007d9c] font-medium">
+                  Go
+                </span>
+              </Link>
               ,{' '}
-              <a href="https://www.rust-lang.org/">
-                <span className="text-[#aa4422] font-medium">Rust</span>
-              </a>
+              <Link href="https://www.rust-lang.org/">
+                <span className="link-hover bg-gradient-to-r from-[#aa4422] to-[#aa4422] font-medium">
+                  Rust
+                </span>
+              </Link>
               ,{' '}
-              <a href="https://www.typescriptlang.org/">
-                <span className="text-[#007acc] font-medium">Typescript</span>
-              </a>
+              <Link href="https://www.typescriptlang.org/">
+                <span className="link-hover bg-gradient-to-r from-[#007acc] to-[#007acc] font-medium">
+                  Typescript
+                </span>
+              </Link>
               , and{' '}
-              <a href="https://www.python.org/">
-                <span className="text-[#3776ab] font-medium">Python</span>
-              </a>
+              <Link href="https://www.python.org/">
+                <span className="link-hover bg-gradient-to-r from-[#3776ab] to-[#3776ab] font-medium">
+                  Python
+                </span>
+              </Link>
             </li>
             <li className="py-1">
               <span className="text-blue-500 font-semibold">
                 Web Development
               </span>{' '}
               with{' '}
-              <a href="https://reactjs.org/">
-                <span className="text-[#61dafb] font-medium">React</span>
-              </a>
+              <Link href="https://reactjs.org/">
+                <span className="link-hover bg-gradient-to-r from-[#61dafb] to-[#61dafb] font-medium">
+                  React
+                </span>
+              </Link>
               ,{' '}
-              <a href="https://svelte.dev">
-                <span className="text-[#ff3e00] font-medium">Svelte</span>
-              </a>
+              <Link href="https://svelte.dev">
+                <span className="link-hover bg-gradient-to-r from-[#ff3e00] to-[#ff3e00] font-medium">
+                  Svelte
+                </span>
+              </Link>
               , and{' '}
-              <a href="https://www.w3schools.com/html/">
-                <span className="text-[#e44d26] font-medium">HTML</span>
-              </a>
+              <Link href="https://www.w3schools.com/html/">
+                <span className="link-hover bg-gradient-to-r from-[#e44d26] to-[#e44d26] font-medium">
+                  HTML
+                </span>
+              </Link>
               /
-              <a href="https://www.w3schools.com/css/">
-                <span className="text-[#254bdd] font-medium">CSS</span>
-              </a>
+              <Link href="https://www.w3schools.com/css/">
+                <span className="link-hover bg-gradient-to-r from-[#254bdd] to-[#254bdd] font-medium">
+                  CSS
+                </span>
+              </Link>
             </li>
             <li className="py-1">
               <span className="text-blue-500 font-semibold">Networking</span>{' '}
               with{' '}
-              <a href="https://www.cloudflare.com">
-                <span className="text-[#f6821f] font-medium">Cloudflare</span>
-              </a>
+              <Link href="https://www.cloudflare.com">
+                <span className="link-hover bg-gradient-to-r from-[#f6821f] to-[#f6821f] font-medium">
+                  Cloudflare
+                </span>
+              </Link>
               ,{' '}
-              <a href="https://vercel.com">
-                <span className="text-black font-medium">Vercel</span>
-              </a>
+              <Link href="https://vercel.com">
+                <span className="link-hover bg-gradient-to-r from-black to-black font-medium">
+                  Vercel
+                </span>
+              </Link>
               , and{' '}
-              <a href="https://www.netlify.com/">
-                <span className="text-[#3dc9c9] font-medium">Netlify</span>
-              </a>
+              <Link href="https://www.netlify.com/">
+                <span className="link-hover bg-gradient-to-r from-[#3dc9c9] to-[#3dc9c9] font-medium">
+                  Netlify
+                </span>
+              </Link>
             </li>
           </ul>
         </div>
@@ -99,24 +137,29 @@ export function Index() {
           </p>
           <ul className="list-style">
             <li className="py-1">
-              <span className="link-hover">
-                <Link href="https://discode.mazylol.com">Discode</Link>
+              <span className="link-hover bg-gradient-to-r from-red-500 to-red-500">
+                <Link href="https://discode.mazylol.com">
+                  <span className="font-semibold">Discode</span>
+                </Link>
               </span>
-              : My Discord bot that gives programming info
+              : My discord bot that gives programming info
             </li>
             <li className="py-1">
-              <span className="link-hover">
-                <Link href="https://github.com/mazylol/glitter">Glitter</Link>
+              <span className="link-hover bg-gradient-to-r from-red-500 to-red-500">
+                <Link href="https://github.com/mazylol/glitter">
+                  <span className="font-semibold">Glitter</span>
+                </Link>
               </span>
               : A Git wrapper that speeds up some of my daily tasks
             </li>
             <li className="py-1">
-              <span className="link-hover">
+              <span className="link-hover bg-gradient-to-r from-red-500 to-red-500">
                 <Link href="https://github.com/warper-development/homepage">
-                  Warper
+                  <span className="font-semibold">Warper</span>
                 </Link>
               </span>
-              : A possible discord alternative with a focus on privacy (very alpha)
+              : A possible discord alternative with a focus on privacy (very
+              alpha)
             </li>
           </ul>
         </div>
@@ -126,18 +169,24 @@ export function Index() {
           <p className="inner-title group-hover:block">See More Of Me</p>
           <ul className="list-style">
             <li className="py-1">
-              <span className="link-hover">
-                <Link href="https://github.com/mazylol">Github</Link>
+              <span className="link-hover bg-gradient-to-r from-green-500 to-green-500">
+                <Link href="https://github.com/mazylol">
+                  <span className="font-semibold">Github</span>
+                </Link>
               </span>
             </li>
             <li className="py-1">
-              <span className="link-hover">
-                <Link href="https://twitter.com/mazylol">Twitter </Link>
+              <span className="link-hover bg-gradient-to-r from-green-500 to-green-500">
+                <Link href="https://twitter.com/mazylol">
+                  <span className="font-semibold">Twitter</span>
+                </Link>
               </span>
             </li>
             <li className="py-1">
-              <span className="link-hover">
-                <Link href="mailto:mazy@cock.li">Email</Link>
+              <span className="link-hover bg-gradient-to-r from-green-500 to-green-500">
+                <Link href="mailto:mazy@cock.li">
+                  <span className="font-semibold">Email</span>
+                </Link>
               </span>
             </li>
           </ul>
